d94d8124ffa249155a4049bde453d42e57e9f081
Carl Custav Mäeorg <carlmaeorg@hotmail.com>
2017-05-06 16:11:31 +0300

    read quotes from json

--- myapp/views.py
+++ myapp/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+import json
+from .models import Sentence
+
+def readJSONToDatabase():
+    with open('quotes.json') as data_file:
+        data = json.load(data_file)
+
+    for line in data:
+        Sentence(sentence=line['quote'],author=line['author']).save()
